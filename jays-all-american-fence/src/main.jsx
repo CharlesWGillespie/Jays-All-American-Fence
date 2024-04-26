19341a7027b8c505af8174835f81e32f0996c28b
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-04-26 11:49:29 -0400

    added hompegae

--- jays-all-american-fence/src/main.jsx
+++ jays-all-american-fence/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx'
-import Homepage from './HomePage.jsx'
+import Homepage from './pages/homepage.jsx'
 
 const router = createBrowserRouter([
   {
