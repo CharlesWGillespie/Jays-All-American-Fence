f8071699cd512df14178d9ac9aba3694f8ac3ee8
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-04-26 11:26:46 -0400

    started project for jays all american fence/ set up react app

---
+++ jays-all-american-fence/src/main.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx'
+import Homepage from './HomePage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: '/homepage',
+        element: <Homepage />
+      }
+      
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
